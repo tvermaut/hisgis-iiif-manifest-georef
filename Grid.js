e8d15125fcc67e03b6850c39cb2aca2f8d1ad22a
Thomas Vermaut <40355758+tvermaut@users.noreply.github.com>
2025-03-03 13:31:50 +0100

    laatste bounds correctie

--- Grid.js
+++ Grid.js
@@ -64,20 +64,20 @@ class Grid {
         };
 
         // Bereken het startpunt voor het grid (gecentreerd)
-        const startX = centerX - Math.floor((this.imageBounds.max.x - this.imageBounds.min.x) / (2 * this.distance.berekend)) * this.distance.berekend;
-        const startY = centerY - Math.floor((this.imageBounds.max.y - this.imageBounds.min.y) / (2 * this.distance.berekend)) * this.distance.berekend;
+        const startX = centerX - Math.floor((this.imageBounds._northEast.lat) / (2 * this.distance.berekend)) * this.distance.berekend;
+        const startY = centerY - Math.floor((this.imageBounds._northEast.lng) / (2 * this.distance.berekend)) * this.distance.berekend;
 
         // Teken verticale lijnen
-        for (let x = startX; x <= this.imageBounds.max.x; x += this.distance.berekend) {
-            const start = rotatePoint(x, this.imageBounds.min.y);
-            const end = rotatePoint(x, this.imageBounds.max.y);
+        for (let x = startX; x <= this.imageBounds._northEast.lat; x += this.distance.berekend) {
+            const start = rotatePoint(x, 0);
+            const end = rotatePoint(x, this.imageBounds._northEast.lng);
             L.polyline([[start.y, start.x], [end.y, end.x]], { color: 'rgba(255, 0, 0, 0.5)', weight: 1 }).addTo(gridLayer);
         }
 
         // Teken horizontale lijnen
-        for (let y = startY; y <= this.imageBounds.max.y; y += this.distance.berekend) {
-            const start = rotatePoint(this.imageBounds.min.x, y);
-            const end = rotatePoint(this.imageBounds.max.x, y);
+        for (let y = startY; y <= this.imageBounds._northEast.lng; y += this.distance.berekend) {
+            const start = rotatePoint(0, y);
+            const end = rotatePoint(this.imageBounds._northEast.lat, y);
             L.polyline([[start.y, start.x], [end.y, end.x]], { color: 'rgba(255, 0, 0, 0.5)', weight: 1 }).addTo(gridLayer);
         }
     }
